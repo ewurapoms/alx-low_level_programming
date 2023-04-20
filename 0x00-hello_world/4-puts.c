accbec51ac52c6cbcde4d3a468e2e8c3b8bea522
ewurapoms <apowurah@outlook.com>
2023-04-20 10:52:00 -0700

    Hello, Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main: point of input
+ *
+ * return: print 0 as (correct)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
