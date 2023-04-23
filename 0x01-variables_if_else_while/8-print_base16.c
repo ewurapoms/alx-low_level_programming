9b92f720f74cb656b8beb648347e9701b95d4927
ewurapoms <apowurah@outlook.com>
2023-04-23 07:43:24 -0700

    Base 16 lowercase

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,9 +2,19 @@
 
 /**
  * main - Entry point
- * Description: playing with base 10 numbers 
+ * Description: prints all  base 16 numbers in lowercase
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int base = 48;
+	int bl;
+	/*where bl = base 16 in lowercase*/
+
+	for (bl = 48; bl < 58; bl++)
+		putchar(bl);
+	/*consider 97 and 103 as ASCII reps of a, and g*/
+	for (bl = 97; bl < 103; bl++)
+		putchar(bl);
+	putchar('\n');
+	return (0);
+}
