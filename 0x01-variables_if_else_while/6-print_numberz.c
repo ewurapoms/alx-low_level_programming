4ef0c3f543add1272b235ee11068b9af20bba6a2
ewurapoms <apowurah@outlook.com>
2023-04-23 05:48:50 -0700

    Putchar PrintNum

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,16 +2,19 @@
 
 /**
  * main - Entry point
- * Description:output base 10 number with putchar 
+ * Description: output base 10 number with putchar
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-        int b = 0;
+	int b = 0;
 
-        while (b <= 9)
-		putchar(b + '0');
-	b++
-	putchar('\n);
+	while
+		(b <= 9)
+		{
+			putchar(b + '0');
+			b++;
+		}
+	putchar('\n');
 	return (0);
 }
