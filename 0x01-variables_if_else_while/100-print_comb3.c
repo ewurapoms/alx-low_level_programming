ed09cd9e6901b00aa55b15f363bda009148201b7
ewurapoms <apowurah@outlook.com>
2023-04-24 10:49:17 -0700

    lowercase and UPPERCASE

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: display all possible different combinations of two digits
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ;
+
+	for ()
+	{
+
+	putchar('\n');
+	return (0);
+}
