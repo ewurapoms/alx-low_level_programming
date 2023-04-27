32b3baa03568343eec27399b09c18c094e7fe10b
ewurapoms <apowurah@outlook.com>
2023-04-27 02:56:59 -0700

    More Functions and Nested Loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase letters
+ * @u: the value to check for
+ * Return: 1 for upper, else 0
+ */
+
+int_isupper(int u)
+{
+	if (u >= 65 && u <= 90)
+	/* read 'u' values as ascii char A and Z */
+	{
+		return (1);
+	}
+		return (0);
+}
