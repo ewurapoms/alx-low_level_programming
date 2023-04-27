b54a445fee9997e89988c963df5ab01642470d88
ewurapoms <apowurah@outlook.com>
2023-04-27 04:45:48 -0700

    Multipy Integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - refers the multiplication of two integers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: print output
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
