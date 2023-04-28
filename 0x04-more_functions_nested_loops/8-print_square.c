ca2186c95ed6f3cb9c78b25ffb95cc4bf7722335
ewurapoms <apowurah@outlook.com>
2023-04-27 17:47:48 -0700

    Print squares

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,19 +1,29 @@
 #include "main.h"
 
 /**
- * print_square - print square with char #
- * @size: represents square
+ * print_square - prints a square
+ * @size: as size of square
  * Return: 0
  */
 
 void print_square(int size)
 {
-	int q, r;
-
-	for (q = 1; q <= size; q++)
+	if (size <= 0)
 	{
-		for (r = 1; r <= size; r++)
-			_putchar('#');
 		_putchar('\n');
+	}
+	else
+	{
+		int q;
+		int r;
+
+		for (q = 0; q < size; q++)
+		{
+			for (r = 0; r < size; r++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
 	}
 }
