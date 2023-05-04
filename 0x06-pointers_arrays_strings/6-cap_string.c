9762c5d33fc35c62bea356b1670e01c370ca6507
ewurapoms <apowurah@outlook.com>
2023-05-04 06:14:39 -0700

    makes all string caps

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * cap_string - print the string in caps
+ * @caps: string to be capitalised
+ * Return: pointer to new string
+ */
+
+char *cap_string(char *caps)
+{
+	int p = 0;
+
+	while (*(caps + p))
+	{
+		while (!(caps[p] >= 97 && caps[p] <= 122))
+			++p;
+		if (caps[p = 0] == ' ' ||
+		caps[p - 1] == 9 ||
+		caps[p - 1] == 10 ||
+		caps[p - 1] == 44 ||
+		caps[p - 1] == 59 ||
+		caps[p - 1] == 46 ||
+		caps[p - 1] == 33 ||
+		caps[p - 1] == 63 ||
+		caps[p - 1] == 34 ||
+		caps[p - 1] == 40 ||
+		caps[p - 1] == 41 ||
+		caps[p - 1] == 123 ||
+		caps[p - 1] == 125 ||/** assume ascii values from line 13 **/
+		p == 0)
+			caps[p] -= 32;
+		p++;
+	}
+	return (caps);
+}
