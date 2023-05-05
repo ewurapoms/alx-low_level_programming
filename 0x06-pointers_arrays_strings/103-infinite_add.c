e6e8a0284f06e00b541e225d598d4067693ec12e
ewurapoms <apowurah@outlook.com>
2023-05-04 18:04:58 -0700

    Adds two numbers

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -11,41 +11,41 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int a = 0, b = 0, c, d, e, f, g = 0;
+	int a = 0, b = 0, ab, cd, ef, gh, xy = 0;
 
-	while (n1[a] != '\0')
+	while (*(n1 + a) != '\0')
 		a++;
 	while (*(n2 + b) != '\0')
 		b++;
 	if (a >= b)
-		d = a;
+		cd = a;
 	else
-		d = b;
-	if (size_r <= d + 1)
+		cd = b;
+	if (size_r <= cd + 1)
 		return (0);
-	r[d + 1] = '\0';
+	r[cd + 1] = '\0';
 	a--, b--, size_r--;
-	e = *(n1 + a) - '0', f = *(n2 + b) - '0';
-	while (d >= 0)
+	ef = *(n1 + a) - 48, gh = *(n2 + b) - 48;
+	while (cd >= 0)
 	{
-		c = e + f + g;
-		if (c >= 10)
-			g = c / 10;
+		ab = ef + gh + xy;
+		if (ab >= 10)
+			xy = ab / 10;
 		else
-			g = 0;
-		if (c > 0)
-		*(r + d) = (c % 10) + 48;
+			xy = 0;
+		if (ab > 0)
+		*(r + cd) = (ab % 10) + 48;
 		else
-			*(r + d) = '0';
+			*(r + ef) = 48;
 		if (a > 0)
-			a--, e = *(n1 + a) - 48;
+			a--, ef = *(n1 + a) - 48;
 		else
-			a = 0;
+			ef = 0;
 		if (b > 0)
-			b--, f = *(n2 + b) - 48;
+			b--, gh = n2[b] - 48;
 		else
-			f = 0;
-		d--, size_r--;
+			gh = 0;
+		cd--, size_r--;
 	}
 	if (*(r) == '0')
 		return (r + 1);
