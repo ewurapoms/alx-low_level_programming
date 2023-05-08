4f9908161d31510d3cd4410d3e1b1fcb2cc4a531
ewurapoms <apowurah@outlook.com>
2023-05-08 13:13:20 -0700

    Set String

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - sets value of pointer to a char
+ * @s: is a double pointer 
+ * @to: input value
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
