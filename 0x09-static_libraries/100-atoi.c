de35ba1dd4694a53140b3ed3bcbec7c7503e4d22
ewurapoms <apowurah@outlook.com>
2023-05-12 14:32:12 -0700

    Libmy task

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,10 +1,23 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
+
+/**
+ * _atoi - changes a string to an int
+ * @s: the converting pointer
+ * Return: int
+ */
 
 int _atoi(char *s)
 {
-        return atoi(s);
+	unsigned int m = 0;
+	int n = 1;
+
+	do {
+		if (*s == '-')
+			n *= -1;
+		else if (*s >= 48 && *s <= 57)
+			m = (m * 10) + (*s - 48);
+		else if (m > 0)
+			break;
+	} while (*s++);
+	return (m * n);
 }
