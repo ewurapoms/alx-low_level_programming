2c1093a6e2935bfce9868518935ddffcc520dc74
ewurapoms <apowurah@outlook.com>
2023-05-18 05:47:47 -0700

    function that reallocates a memory block

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,5 +1,43 @@
 #include "main.h"
-#include <stdlib.>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _realloc - function that reallocates a memory block
+ * @ptr: is a pointer
+ * @old_size: initial memory size
+ * @new_size: size memory to print
+ *
+ * Return: a pointer to reallocated memory and free ptr
+ */
+
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *pt;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (ptr == NULL)
+	{
+		pt = malloc(new_size);
+		if (pt == 0)
+			return (NULL);
+		free(ptr);
+		return (pt);
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	pt = malloc(new_size);
+	if (pt == 0)
+		return (NULL);
+	_memcpy(pt, ptr, old_size);
+	free(ptr);
+	return (pt);
+}
 
 /**
  * _memcpy - function that copies memory area
@@ -19,4 +57,3 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	}
 	return (dest);
 }
-
