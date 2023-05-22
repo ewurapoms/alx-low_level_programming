8e58fa341aaefe9377b2b2fff5212ea725190b70
ewurapoms <apowurah@outlook.com>
2023-05-22 04:55:37 -0700

    Task 0 Header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
+/**
+ * struct dog - Entry point
+ * @name: is a pointer to a char
+ * @age: is a pointer to a float
+ * @owner: is another pointer to a char
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
 
 #endif
