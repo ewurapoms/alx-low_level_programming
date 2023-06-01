890ca2da8fd334c2cd209f168516e54d184dd65c
ewurapoms <apowurah@outlook.com>
2023-06-01 11:28:52 -0700

    tasks update

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-#include <stdio.h>
 
 /**
  * print_list - prints the string length
@@ -15,10 +14,9 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 		if (h->str == NULL)
-			printf("[0] (nil)\n");
+			printf("[0](nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
-
 		h = h->next;
 		nodes++;
 	}
