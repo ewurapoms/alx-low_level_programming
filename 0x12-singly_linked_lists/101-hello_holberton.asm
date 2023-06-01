af990d747abaa8459db707b1233088d5bb780ada
ewurapoms <apowurah@outlook.com>
2023-06-01 08:48:40 -0700

    assembly prog that prints a string

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,16 @@
 section .text
-  global main
+global main
 
+section .rodata
+    msg: db "Hello, Holberton", 10
+    msglen: equ $ - msg
 main:
     mov rax, 1
     mov rdi, 1
     mov rsi, msg
-    mov rdx, msg len
+    mov rdx, msglen
     syscall
 
     mov rax, 60
     mov rdi, 0
     syscall
-
-section .rodata
-    msg: db "Hello, Holberton", 10
-    msglen: equ $ - msg
