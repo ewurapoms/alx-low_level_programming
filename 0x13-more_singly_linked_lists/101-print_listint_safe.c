81284260f1abd6c69efcba16deac8fe8b26bf22f
ewurapoms <apowurah@outlook.com>
2023-06-06 00:26:00 -0700

    function prints safe version of a list

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - prints all the values of a listint_t list
+ * @head: is a pointer
+ *
+ * Return: output.
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t show = 0;
+
+	if (head == NULL)
+		return (0);
+
+	while (head != NULL)
+	{
+		printf("[%p] %d\n", (void *)head, head->n);
+		show++;
+		if (head->next < head)
+		{
+			head = head->next;
+		}
+		else
+		{
+			head = head->next;
+			printf("-> [%p] %d\n", (void *)head, head->n);
+			break;
+		}
+	}
+	return (show);
+}
